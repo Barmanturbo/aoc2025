0e2cd65fed403d1a98fac4e9795bd1cb4812a8bc
Barmanturbo <21035407@student.hhs.nl>
2025-12-01 21:51:27 +0100

    aoc 1 december

---
+++ 1_december/combination_lock.py
@@ -0,0 +1,43 @@
+dial = 50
+
+password = 0
+
+def rotate(direction, amount):
+    global dial, password
+    if direction == "R":
+        while amount > 0:
+            dial += 1
+
+            if dial == 0:
+                password += 1
+
+            if dial > 99:
+                dial = 0
+                password += 1
+            amount -= 1
+
+    elif direction == "L":
+        while amount > 0:
+            dial -= 1
+            
+            if dial == 0:
+                password += 1
+
+            if dial < 0:
+                dial = 99
+
+            amount -= 1
+
+            
+# >6035; <8026
+
+
+with open("1_december/december1_input.txt") as file:
+    for line in file:
+        direction = line[0]
+        amount = int(line[1:].strip())
+        print(f"Rotating {direction} by {amount} from {dial}")
+        rotate(direction, amount)
+
+
+print(f"PASSWORD: {password}")
